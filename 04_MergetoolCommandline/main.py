f65f040003f0c7a1ad05eb3663cbcd1c635a1f51
stamplevskiyd <stamplevskiyd@gmail.com>
2022-03-11 16:28:15 +0300

    Fixed name generator

--- 04_MergetoolCommandline/main.py
+++ 04_MergetoolCommandline/main.py
@@ -1,8 +1,9 @@
 import cmd
 import shlex
-import pynames
 import inspect
 import readline
+import pynames
+from pynames import GENDER, LANGUAGE
 
 
 class Name_cmd(cmd.Cmd):
@@ -18,18 +19,25 @@ class Name_cmd(cmd.Cmd):
         if not (race in allowed_races):
             print("Names for this race can not be generated")
             return
-        generator_name, gender = None, 'male'
+        gen_name, gender = '', 'male'
         if len(args) > 1:
             if args[1] == 'male' or args[1] == 'female':
                 gender = args[1]
             else:
-                generator_name = args[1]
+                gen_name = args[1]
             if len(args) == 3:
                 gender = args[2]
         start_line = 'pynames.generators.' + race
         base = eval(start_line)
         members_list = inspect.getmembers(base)
-        print(members_list)
+        generator_name = [elem[0] for elem in members_list if
+                          elem[0].endswith("Generator") and elem[0].startswith(gen_name)
+                          and elem[0] != 'FromListGenerator'][0]  # генератор из списка, которого у нас нет
+        gender_str = 'GENDER.' + gender.upper()
+        start_line += '.' + generator_name + '()'
+        generator_object = eval(start_line)
+        print(generator_object.get_name(eval(gender_str)))
+
 
 
 Name_cmd().cmdloop()
